cf7a85a23055ee39308ef2e5885962051d897c57
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-01 00:43:55 -0300

    feat: add ITraceManager

---
+++ src/MCB.Core.Infra.CrossCutting.Observability.Abstractions/ITraceManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+
+namespace MCB.Core.Infra.CrossCutting.Observability.Abstractions;
+
+public interface ITraceManager
+{
+    void StartActivity(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Action<Activity> handler,
+        CancellationToken cancellationToken
+    );
+    void StartActivity<TInput>(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Action<TInput, Activity> handler,
+        CancellationToken cancellationToken
+    );
+    void StartActivity<TInput, TOutput>(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Func<TInput, Activity, TOutput> handler,
+        CancellationToken cancellationToken
+    );
+
+    Task StartActivityAsync(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Func<Activity, Task> handler,
+        CancellationToken cancellationToken
+    );
+    Task StartActivityAsync<TInput>(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Func<TInput, Activity, Task> handler,
+        CancellationToken cancellationToken
+    );
+    Task StartActivityAsync<TInput, TOutput>(
+        string name,
+        ActivityKind kind,
+        Guid tenantId,
+        string executionUser,
+        string sourcePlatform,
+        Func<TInput, Activity, Task<TOutput>> handler,
+        CancellationToken cancellationToken
+    );
+}
