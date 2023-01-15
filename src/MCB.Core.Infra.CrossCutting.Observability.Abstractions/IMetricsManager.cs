88ba3b78cd466efef31824bbb580d7220cffdf0f
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2023-01-15 13:59:51 -0300

    refact: add name in RecordHistogram

--- src/MCB.Core.Infra.CrossCutting.Observability.Abstractions/IMetricsManager.cs
+++ src/MCB.Core.Infra.CrossCutting.Observability.Abstractions/IMetricsManager.cs
@@ -6,13 +6,13 @@ namespace MCB.Core.Infra.CrossCutting.Observability.Abstractions;
 public interface IMetricsManager
 {
     void CreateCounter<T>(string name, string? unit = null, string? description = null) where T : struct;
-    IEnumerable<Counter> GetCounterCollection();
     void IncrementCounter<T>(string name, T delta) where T : struct;
     void IncrementCounter<T>(string name, T delta, params KeyValuePair<string, object?>[] tags) where T : struct;
+    IEnumerable<Counter> GetCounterCollection();
 
     void CreateHistogram<T>(string name, string? unit = null, string? description = null) where T : struct;
+    void RecordHistogram<T>(string name, T value, KeyValuePair<string, object?> tag) where T : struct;
     IEnumerable<Histogram> GetHistogramCollection();
-    void RecordHistogram<T>(T value, KeyValuePair<string, object?> tag) where T : struct;
 
     void CreateObservableGauge<T>(string name, Func<IEnumerable<Measurement<T>>> observeValues, string? unit = null, string? description = null) where T : struct;
     IEnumerable<ObservableGauge> GetObservableGaugeCollection();
